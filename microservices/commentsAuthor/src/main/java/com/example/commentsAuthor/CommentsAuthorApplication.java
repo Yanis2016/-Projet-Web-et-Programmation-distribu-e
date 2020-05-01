53242d8394f5488467c2ffe55d67a7f1fb90d019
Yanis AIT HAMMOU <60472357+Yanis2016@users.noreply.github.com>
2020-05-01 18:51:34 +0200

    Add files via upload

---
+++ microservices/commentsAuthor/src/main/java/com/example/commentsAuthor/CommentsAuthorApplication.java
@@ -0,0 +1,13 @@
+package com.example.commentsAuthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsAuthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsAuthorApplication.class, args);
+	}
+
+}
---
+++ microservices/commentsAuthor/src/test/java/com/example/commentsAuthor/CommentsAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commentsAuthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsAuthorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
